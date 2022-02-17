f346cc00d96633b3adde78ff527ae1932760f199
aidaikin <aidaikin.a@gmail.com>
2022-02-17 14:58:40 -0500

    Line Formatting

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,22 +1,19 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-        String phraseOne = "First";
-        String phraseTwo = "Second";
 
         String result = returnString(50);
         System.out.println(result);
 
-        System.out.println("Hello " + phraseOne + " Phrase!");
-        System.out.println("Hello " + phraseTwo + " Phrase!");
-        System.out.println("How cool to have " + phraseOne + " and " + phraseTwo + " here all together :)");
+        System.out.printf("Hello, %s", " First Phrase!");
+        System.out.printf("Hello, %s", " Second Phrase");
+
 
         SecondClass secondClassInstance = new SecondClass();
-
         System.out.println(secondClassInstance.returnHello());
     }
 
-    public static String returnString(int x){
+   public static String returnString(int x){
         return " Hi Aidai";
     }
 }
